204ea060bdb27d1194a9a7787ec80e4e770f8be9
Christian Chicoine <christian.chicoine@gmail.com>
2018-01-03 12:50:25 -0500

    added swagger

--- CorpocastFAQApi/Startup.cs
+++ CorpocastFAQApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using CorpocastFAQApi.Models;
 using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CorpocastFAQApi
 {
@@ -33,6 +34,13 @@ namespace CorpocastFAQApi
             {
                 options.SerializerSettings.ContractResolver = new DefaultContractResolver();
             });
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Corpocast FAQ API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +50,15 @@ namespace CorpocastFAQApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Corpocast FAQ API V1");
+            });
 
             app.UseMvc();
         }
